9e8b2ff120632bda13708c07476061169a3f9a8e
maskhed <maskhed@126.com>
2018-10-17 15:47:23 +0800

    add Hawkeye

---
+++ Hawkeye/client/src/components/NavMenu.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="navigation">
+        <el-menu
+                background-color="#24292e"
+                text-color="rgba(255, 255, 255, 0.75)"
+                active-text-color="#fff"
+                :default-active="activeIndex"
+                router
+                mode="horizontal">
+            <el-menu-item index="" disabled>
+                <img src="./../assets/github.svg" style="font-size: 20px;width: 3em;height: 1.5em;" alt="">
+                GitHub 监控平台
+            </el-menu-item>
+            <el-menu-item index="/">
+                概览
+            </el-menu-item>
+            <el-menu-item index="/setting/rule">
+                配置
+            </el-menu-item>
+
+        </el-menu>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        data() {
+            return {};
+        },
+        computed: {
+            activeIndex() {
+                return this.$route.name;
+            }
+        },
+        mounted: function () {
+            this.$nextTick(function () {
+            });
+        }
+    };
+</script>
+
+<style>
+    .el-menu-item a {
+        text-decoration: none;
+        display: block;
+    }
+</style>
---
+++ Hawkeye/client/src/main.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@/config/api";
+import axios from "axios";
+import VueHighlightJS from "vue-highlight.js";
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import { b64Decode, timeAgo, toThousands, dateFormat } from "./utils";
+
+Vue.filter("b64Decode", b64Decode);
+Vue.filter("timeAgo", timeAgo);
+Vue.filter("dateFormat", dateFormat);
+Vue.filter("toThousands", toThousands);
+Vue.config.productionTip = false;
+Vue.use(VueHighlightJS);
+Vue.use(Element);
+Vue.prototype.axios = axios;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
